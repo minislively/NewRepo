097e86bfe9047518ab3de450acb0dcd5f684de51
김민설 <sri07091@naver.com>
2021-09-01 16:11:05 +0900

    백준 17256번

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a1, b1, c1;
+	int a3, b3, c3;
+
+	scanf("%d %d %d", &a1, &b1, &c1);
+	scanf("%d %d %d", &a3, &b3, &c3);
+
+	printf("%d %d %d", a3 - c1, b3 / b1, c3 - a1);
+
+}
