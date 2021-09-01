aaf12cc29a4e1412623894f03b056548b8bf78b2
김민설 <sri07091@naver.com>
2021-09-01 16:48:32 +0900

    백준 15439번

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n, num = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) // 색깔 겹치지 않게
+	{
+		for (int j = i + 1; j <= n; j++) // 얘도 
+		{
+			i* j;
+			num++;
+		}
+	}
+	printf("%d", 2 * num); // 2는 바지, 티셔츠 숫자 
+}
