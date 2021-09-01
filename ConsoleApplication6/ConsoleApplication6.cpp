8510dc3ff1d19ae4f369f5a493efc695a5fc5e4a
김민설 <sri07091@naver.com>
2021-09-01 16:21:54 +0900

    백준 17496번

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n, t, c, p, num = 0;
+
+	scanf("%d %d %d %d", &n, &t, &c, &p);
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (t * i < n) // 1<=t<=n-1, n일이 지나면 바로 시들어버림
+			num = num + c;
+
+		else
+			break;
+	}
+	printf("%d", num * p);
+}
