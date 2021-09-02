405b8ffe5994aa42939aeb0f029157a7f3270001
김민설 <sri07091@naver.com>
2021-09-02 14:28:39 +0900

    백준 2752번

---
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int arr[3];
+	int temp = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i + 1; j < 3; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
