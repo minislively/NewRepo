c9b64719fd4a9596b2ce8e1f18c88fbc687a1760
김민설 <sri07091@naver.com>
2021-09-02 17:42:35 +0900

    백준 2566번

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	int arr[9][9];
+	int max = 0;
+	int n = 0, m = 0;
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (max < arr[i][j])
+			{
+				max = arr[i][j];
+				m = i + 1;
+				n = j + 1;
+			}
+		}
+	}
+	printf("%d\n", max);
+	printf("%d %d", m, n);
+}
