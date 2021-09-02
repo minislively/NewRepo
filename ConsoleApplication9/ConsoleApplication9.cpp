a709dc81452a2e890fd393aa9551d7049a93b47b
김민설 <sri07091@naver.com>
2021-09-02 09:39:45 +0900

    깊은 복사 예제

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	
+	scanf("%d %d %d", &a, &b, &c);
+
+	while (1)
+	{
+		if (b + c >= 60)
+			printf("%d %d", a++, 60 - (b + c));
+		if (b + c >= 0 && b + c < 60)
+			break;
+		else
+		{
+			printf("%d %d", a, b + c);
+			break;
+		}
+	}
+}
