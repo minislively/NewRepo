8d51f8dc39e42651a9979a242a826dadf0aac9ee
김민설 <sri07091@naver.com>
2021-09-02 17:25:40 +0900

    백준 10990번

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n >= 2)
+	{
+		for (int i = 1; i < n; i++)
+		{
+			printf(" ");
+		}
+	}
+	if (n >= 1)
+	{
+		printf("*\n");
+	}
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int k = i; k < n; k++)
+		{
+			printf(" ");
+		}
+		printf("*");
+		for (int m = 1; m <= 2 * (i - 1) - 1; m++)
+		{
+			printf(" ");
+		}
+		printf("*\n");
+
+	}
+}
