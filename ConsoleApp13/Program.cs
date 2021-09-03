2020dc4e38b507801d1a1c60196b70c5675621e1
김민설 <sri07091@naver.com>
2021-09-04 06:47:07 +0900

    강아지 클래스 만들기

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Min
+{
+    class Dog
+    {
+        public string name;
+        public string gender;
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} : 멍멍!", name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "무무";
+            dog.gender = "여자";
+
+            dog.Bark();
+            Console.WriteLine("{0} : {1}", dog.name, dog.gender);
+        }
+    }
+}
