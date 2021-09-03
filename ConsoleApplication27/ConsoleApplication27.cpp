cf7f41403363a9f5d6cf913ed1786096e3372317
김민설 <sri07091@naver.com>
2021-09-03 14:18:53 +0900

    코드업 1068번

---
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+
+	if (a >= 90 && a <= 100)
+	{
+		printf("%c", 'A');
+	}
+	else
+	{
+		if (a >= 70 && a <= 89)
+		{
+			printf("%c", 'B');
+		}
+		else
+		{
+			if (a >= 40 && a <= 69)
+			{
+				printf("%c", 'C');
+			}
+			else
+			{
+				if(a>=0 && a<=39)
+					printf("%c", 'D');
+			}
+		}
+	}
+}
