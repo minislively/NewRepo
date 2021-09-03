6d4fa7ba3e0cf63a5b0bab0241806abbfb8feec9
김민설 <sri07091@naver.com>
2021-09-04 06:32:33 +0900

    코드업 1076번

---
+++ ConsoleApplication38/ConsoleApplication38.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    char ch, t = 'a';
+
+    scanf("%c", &ch);
+
+    while (1)
+    {
+
+        printf("%c ", t++);
+
+        if (t > ch)
+            break;
+    }
+}
