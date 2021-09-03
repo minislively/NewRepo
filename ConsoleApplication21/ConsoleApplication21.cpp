e42107c821c2502958ec92bacfc65f4a46e4f8f5
김민설 <sri07091@naver.com>
2021-09-03 14:00:13 +0900

    소멸자 연습

---
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Character
+{
+private:
+	string name;
+	int ragePoint;
+	int hp;
+	int damage;
+
+public :
+	Character(string name, int hp, int damage) : name(name), ragePoint(0), hp(hp), damage(damage){}
+
+	~Character()
+	{
+		cout << "[객체가 소멸됩니다.]\n";
+	}
+	void pointUp() { ragePoint++; }
+	void show()
+	{
+		cout << name << "[" << ragePoint << "] " << hp << " " << damage << "\n";
+	}
+};
+
+int main()
+{
+	Character* slime1 = new Character("슬라임", 10, 20);
+	slime1->pointUp();
+	Character slime2(*slime1);
+	slime2.pointUp();
+	slime1->show();
+
+	delete slime1;
+	system("pause");
+	return 0;
+}
