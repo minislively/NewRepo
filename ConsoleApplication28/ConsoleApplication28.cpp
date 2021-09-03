acf84dc1466ec1d3a8dd640cd870a867f6b3b4f5
김민설 <sri07091@naver.com>
2021-09-03 14:23:20 +0900

    코드업 1069번

---
+++ ConsoleApplication28/ConsoleApplication28.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	char a;
+	scanf("%c", &a);
+
+	switch (a)
+	{
+	case 'A':
+		printf("%s", "best!!!");
+		break;
+	case 'B':
+		printf("%s", "good!!");
+		break;
+	case 'C':
+		printf("%s", "run!");
+		break;
+	case 'D':
+		printf("%s", "slowly~");
+		break;
+	default:
+		printf("%s", "what?");
+		break;
+	}
+}
