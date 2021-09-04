689510a09b04f0bb5e841879d5832cbc6730c121
김민설 <sri07091@naver.com>
2021-09-04 17:43:31 +0900

    선택 정렬 - 알고리즘

---
+++ ConsoleApplication43/ConsoleApplication43.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int i, j, min, index, temp;
+	int arr[10] = { 1,10,5,8,7,6,4,3,2,9 };
+	for (int i = 0; i < 10; i++)
+	{
+		min = 9999;
+		for (int j = i; j < 10; j++)
+		{
+			if (min > arr[j])
+			{
+				min = arr[j];;
+				index = j;
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[index];
+		arr[index] = temp;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
