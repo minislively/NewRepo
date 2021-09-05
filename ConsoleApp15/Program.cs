1f1b483bff8d3ec53ae72ea5766d1665678d7220
김민설 <sri07091@naver.com>
2021-09-05 19:02:48 +0900

    고양이 클래스 예제

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string name;
+        public string color;
+
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 야옹", name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.color = "노란색";
+            kitty.name = "키티";
+            Console.WriteLine("{0}는 {1}이다.", kitty.name, kitty.color);
+
+            Cat nero = new Cat();
+            nero.color = "검은색";
+            nero.name = "네로";
+            Console.WriteLine("{0}는 {1}이다.", nero.name, nero.color);
+
+        }
+    }
+}
