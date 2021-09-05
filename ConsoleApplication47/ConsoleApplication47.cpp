b788b7aa1795876ee30607d5dba67fdecbad182e
minislively <=>
2021-09-05 21:38:36 +0900

    코드업 1078번

---
+++ ConsoleApplication47/ConsoleApplication47.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int sum = 0; 
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+			sum = sum + i;
+	}
+	printf("%d", sum);
+}
