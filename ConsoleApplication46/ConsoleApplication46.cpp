8ba76ba31f813cb3a1f4817289ae0d71b955ed1f
minislively <=>
2021-09-05 21:25:27 +0900

    백준 2750번

---
+++ ConsoleApplication46/ConsoleApplication46.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+	int arr[1000];
+	int temp = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+}
