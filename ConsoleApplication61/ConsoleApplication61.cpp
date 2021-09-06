58f9c6b613b6637a6dac60e7f2b19faccdc4bb95
minislively <=>
2021-09-06 17:45:49 +0900

    백준 20492번

---
+++ ConsoleApplication61/ConsoleApplication61.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+
+	int sum1 = a * 0.22;
+	int sum2 = a * 0.8 + a - (a * 0.8) - (a - a * 0.8) * 0.22;
+
+	printf("%d %d", a - sum1, sum2);
+}
