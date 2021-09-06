ebbb8daae04dd5c7f4b91fa3662bbaa3581eb70c
minislively <=>
2021-09-06 15:21:11 +0900

    코드업 1080번

---
+++ ConsoleApplication54/ConsoleApplication54.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, sum = 0;
+
+	scanf("%d", &a);
+
+	for (int i = 1; i <= a; i++)
+	{
+		sum = sum + i;
+
+		if (sum >= a)
+		{
+			sum = i;
+			break;
+		}
+	}
+	printf("%d", sum);
+}
