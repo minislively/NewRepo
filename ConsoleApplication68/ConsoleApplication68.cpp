183d1732971eeee0e7d3a6616d887b366b2dcb09
minislively <=>
2021-09-07 15:00:39 +0900

    백준 10179번

---
+++ ConsoleApplication68/ConsoleApplication68.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+	double x;
+
+	scanf("%d", &a);
+
+	for (int i = 0; i < a; i++)
+	{
+		scanf("%lf", &x);
+		printf("$%.2lf\n", x - x * 0.2);
+	}
+
+}
