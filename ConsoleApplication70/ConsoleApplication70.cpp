ac0e539c156570ac142e9dd9dc5ba6ca38a19207
minislively <=>
2021-09-07 16:50:58 +0900

    백준 2581번

---
+++ ConsoleApplication70/ConsoleApplication70.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int m, n, cnt = 0, result = 0, min = 10001;
+
+	scanf("%d %d", &m, &n);
+
+	for (int i = m; i <= n; i++)
+	{
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+				cnt++;
+		}
+		if (cnt == 0 && i != 1)
+		{
+			result += i;
+			if (min > i)
+				min = i;
+		}
+		cnt = 0;
+	}
+	if (min == 10001)
+		printf("-1\n");
+	else
+		printf("%d\n%d\n", result, min);
+}
