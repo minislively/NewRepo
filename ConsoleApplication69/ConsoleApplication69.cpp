30d9b658111b1f6a2787c07d54cdecbf7c01b092
minislively <=>
2021-09-07 15:38:39 +0900

    백준 1978번

---
+++ ConsoleApplication69/ConsoleApplication69.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n, num, cnt = 0;
+
+	scanf("%d", &n);
+
+	int i;
+
+	while (n--)
+	{
+		scanf("%d", &num);
+		for (i = 2; i < num; i++)
+		{
+			if (num % i == 0) break;
+		}
+		if (i == num) cnt++;
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
