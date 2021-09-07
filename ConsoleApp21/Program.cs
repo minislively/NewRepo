4af742a1fd44870dd563ea4ac503c70a10c239a8
minislively <=>
2021-09-07 09:24:56 +0900

    익명 함수 예제

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft
+{
+    class MainApp
+    {
+        delegate void TestDelegate<T>(T a, T b);
+        static TestDelegate<int> Lamda;
+
+        static void Main(string[] args)
+        {
+            Lamda += (int a, int b) => Console.WriteLine(a + b);
+            Lamda(1, 2);
+            Console.ReadKey();
+        }
+        static public void TLamda(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
