f1ea2253cf7a620752da7176db46763fe2ee8c0f
minislively <=>
2021-09-07 13:36:13 +0900

    고양이 강아지 클래스 예제

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Min
+{
+    class Dog
+    {
+        public string name;
+        public string gender;
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} : 멍멍!", name);
+        }
+    }
+    class Cat
+    {
+        public string name;
+        public string gender;
+       
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 야옹!", name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "칸";
+            dog.gender = "남자";
+
+            dog.Bark();
+            Console.WriteLine("{0}은 {1}입니다.", dog.name,dog.gender);
+            
+            Cat cat = new Cat();
+            cat.name = "키티";
+            cat.gender = "여자";
+
+            cat.Meow();
+            Console.WriteLine("{0}는 {1}입니다.",cat.name,cat.gender);
+        }
+    }
+}
